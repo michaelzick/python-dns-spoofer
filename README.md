2022c54836571025005e80a3be468f90c93c5684
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-08-15 14:17:43 +0000

    README, added main file, .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# python-dns-spoofer
+# DNS Spoofer in Python
+
+## Part of the Udemy course "Learn Python & Ethical Hacking From Scratch"
---
+++ dns_spoofer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import netfilterqueue
+
+
+def process_packet(packet):
+    print(packet)
+    packet.drop()
+    # packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
